021026bee12dfb2c8261a991b6fdc10bfb5927e2
Andre Monreal-Hernandez <amonreal@example.com>
2026-08-31 18:07:11 -0700

    chore: update author to Andres Monreal-Hernandez and link ORCID 0009-0009-1207-8597 in all citation files

--- qsarcert/__init__.py
+++ qsarcert/__init__.py
@@ -4,7 +4,7 @@ Y-Randomization, and Scaffold Leakage Certification for QSAR & Molecular ML Mode
 """
 
 __version__ = "1.0.0"
-__author__ = "Andre Monreal-Hernández"
+__author__ = "Andres Monreal-Hernández"
 __license__ = "MIT"
 
 from qsarcert.core.applicability_domain import (
